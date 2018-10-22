5f54ccbc64f43ddd2eaa392bddf71584045c2894
Marcelo Camargo <celaummm@gmail.com>
2018-10-22 15:19:52 -0300

    Primeiro commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Administrador master',
+            'email'     => 'admin@admin.com',
+            'password'  => bcrypt('123456'),
+        ]);
+
+        User::create([
+            'name'      => 'Usuário comum',
+            'email'     => 'user@user.com',
+            'password'  => bcrypt('123456'),
+        ]);
+    }
+}
